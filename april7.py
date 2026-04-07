24549b11eaef290649202faef79ffb9819444573
yagitazzy <tazzy.yagi.7@gmail.com>
2026-04-07 10:40:35 -0500

    Add files via upload

--- april7.py
+++ april7.py
@@ -38,6 +38,19 @@ def subtract(num1,num2):
 
 #### TASK 5 (Member #2)- Add the implemented the SUBTRACTION function() to your gitHub repo in a new branch called feature/subtract
 
+#### TASK 7 (Member #3) - Add the implemented the Multiplication function() to your gitHub repo in a new branch called feature/mult
+def mult(num1, num2):
+  print("we are  multiplying" + str(number1) + " and " + str(number2) )
+  return num1 * num2
+
+#### TASK 9 (Member #4)- Add the implemented the Division function() to your gitHub repo in a new branch called feature/div
+def div(num1, num2):
+  print("we are  dividing"+ str(number1) + " and " + str(number2) )
+   if num2 == 0:
+  return "error can't divide by 0"
+  return num1/num2
+  
+
 
 #### TASK 10 (Member #2)- pull the gitHub repo in your local computer  and implement the SQRT function()
 
